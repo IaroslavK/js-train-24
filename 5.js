19161a396211267e5116501a39528cd0ca686754
iaroslavk <iaroslavkhaidakin@gmail.com>
2023-09-04 21:47:13 +0300

    js-train-24

--- 5.js
+++ 5.js
@@ -2,6 +2,27 @@ console.log("Завдання: 5 ==============================");
 
 // Створюємо функцію task5, яка буде використовувати проміси.
 function task5() {
+  let counter = 0;
+  const intervalPromise = new Promise((resolve, reject) => {
+    const element = setInterval(() => {
+      counter++;
+      console.log(`Значення лічильника: ${counter}`);
+
+      if (counter === 5) {
+        clearInterval(element);
+        resolve(counter);
+      }
+    }, 1000);
+  })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((er) => {
+      console.error(er);
+    })
+    .finally(() => {
+      console.log("Завершення лічильника");
+    });
   // Створюємо змінну counter яка буде лічильником та присвоюємо значення 0
   // Створюємо проміс з іменем intervalPromise.
   // Використовуємо функцію setInterval, щоб імітувати асинхронну операцію яка повторюється кожну секунду
